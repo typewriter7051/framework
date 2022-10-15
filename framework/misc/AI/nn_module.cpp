acb74abddbbb85a5c8cf857f831a10a429adf8e3
Typewriter7051 <typewriter7051@gmail.com>
2022-10-15 14:11:26 -0500

    a

--- framework/misc/AI/nn_module.cpp
+++ framework/misc/AI/nn_module.cpp
@@ -1,16 +1,71 @@
+#include <algorithm> // For std::fill()
+
+void NNModule::NNModule() {
+    timesCalled = 0;
+}
+//==============================================================================
+
+int NNModule::getNumInputs() {
+    return inputs->size();
+}
+
+int NNModule::getNumOutputs() {
+    return outputs->size();
+}
+//==============================================================================
+
+void setIOPtrs(std::vector<float>* i, std::vector<float>* o) {
+    inputs = i;
+    outputs = o;
+
+    inputDerivs.resize(i.size());
+    outputDerivs.resize(o->size());
+}
+//==============================================================================
+
+void NNModule::firstCall() {
+    return timesCalled == 1;
+}
+
+void NNModule::lastCall() {
+    return timesCalled == numParents;
+}
+//==============================================================================
+
+void NNModule::trainModule(std::vector<float>* derivs) {
+    timesCalled++;
+    // Reset derivative lists on first call.
+    if (firstCall()) {
+        std::fill(inputDerivs.begin(), inputDerivs.end(), 0);
+        std::fill(outputDerivs.begin(), outputDerivs.end(), 0);
+    }
+    // Add derivs onto outputDerivs.
+    for (float f : derivs)
+        outputDerivs += f;
+    // If all dependencies are solved then pass to train() and train child modules.
+    if (lastCall()) {
+        train(derivs);
+
+        for (NNModule* cm : childModules)
+            cm->trainModule(&inputDerivs);
+    }
+}
+//==============================================================================
+
 // TODO: this alg doesn't work with 2 inputs 1 output!
 void NNModule::updateValues() {
     // Prevents the module from being run twice.
-    if (alreadyUpdated) return;
-    alreadyUpdated = true;
+    timesCalled++;
+    if (!firstCall()) return;
 
-    for (NNModule* cm : connectedModules)
+    for (NNModule* cm : childModules)
         cm->updateValues();
     
-    process();
+    process(inputs, outputs);
 }
+//==============================================================================
 
 void NNModule::callReset() {
-    alreadyUpdated = false;
+    timesCalled = 0;
     reset();
 }
