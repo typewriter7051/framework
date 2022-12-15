b874adc97402c40889f689eb6f0b6f59da5a5ee4
Typewriter7051 <typewriter7051@gmail.com>
2022-12-14 21:34:53 -0600

    a

--- misc/LA/matrix.h
+++ misc/LA/matrix.h
@@ -1,53 +1,158 @@
-//================================================================================
-// Class Definition
+using namespace LA {
 
 class Matrix {
 public:
+    //==========================================================================
+    // Constructors.
+    Matrix() {}
 
-    Matrix();
-    Matrix(unsigned short, unsigned short);
-    Matrix(unsigned short[]);
+    Matrix(int i, int j) {
+        elements = float[i][j];
+    }
 
-    float get(unsigned short yp, unsigned short xp);
-    float getDimensions();
+    Matrix(float[][] data) {
+        elements = data;
+    }
 
-    void set(unsigned short yp, unsigned short xp, float val);
+    Matrix(int m, int n, float* data) {
+        elements = float[m][n];
 
-private:
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                elements[i][j] = data[m * i + j];
+            }
+        }
+    }
+    //==========================================================================
+    // Element-wise operator overloads.
 
-    bool checkInBounds(unsigned short yp, unsigned short xp);
+    Matrix operator + (float c) {
+        return operation(c, 'a');
+    }
+
+    Matrix operator - (float c) {
+        return operation(c, 's');
+    }
+
+    Matrix operator * (float c) {
+        return operation(c, 'm');
+    }
+
+    Matrix operator / (float c) {
+        return operation(c, 'd');
+    }
+    //==========================================================================
+    // Operations.
+
+    // Returns 2 numbers [# of rows, # of cols]
+    int[] getSize() {
+        int size[2];
+        size[0] = sizeof(A) / sizeof(A[0]);
+        size[1] = sizeof(A[0]) / sizeof(A[0][0]);
+
+        return size;
+    }
+
+    Matrix getTranspose() {
+        int[] size = getSize();
+        // Create a matrix with inverted dimensions.
+        Matrix A(size[1], size[0]);
+
+        for (int i = 0; i < size[0]; i++) {
+            for (int j = 0; j < size[1]; j++) {
+                A[j][i] = elements[i][j];
+            }
+        }
+
+        return A;
+    }
+
+    Matrix removeRowAndCol(int r, int c) {
+        int size[] = getSize();
+        Matrix A(size[0] - 1, size[1] - 1);
+
+        for (int i = 0; i < size[0] - 1; i++) {
+            for(int j = 0; j < size[1] - 1; j++) {
+                A[i][j] = elements[i + (i >= r)][j + (j >= c)];
+            }
+        }
+
+        return A;
+    }
+
+    // If checkZeros is true, the matrix will be searched to see which row/col
+    // has the most 0's to iterate through. This can save some time in sparse matrices.
+    float determinant(bool checkZeros) {
+        int[] size = getSize();
+        // iterpos keeps track of which row/col to iterate through.
+        // This is how each row/col is mapped to index in a m by n matrix.
+        /*
+          m . . . m+n-1
+        0
+        .   .     .
+        .     .   .
+        .       . .
+      m-1   . . .
+        */
+        int iterpos = 0;
+        if (checkZeros) {
+            // Keep track of how many zeros in each row/col.
+            int numZeros[size[0] + size[1]];
+
+            for (int i = 0; i < size[0]; i++) {
+                for (int j = 0; j < size[1]; j++) {
+                    bool isZero = (elements[i][j] == 0);
+                    numZeros[i] += isZero;
+                    numZeros[size[0] + j] += isZero;
+                }
+            }
+
+            // Set interpos to the row/col with the most 0's.
+            int maxIdx = 0;
+            for (int i = 0; i < size[0] + size[1]; i++) {
+                if (numZeros[i] > numZeros[maxIdx]) {
+                    maxIdx = i;
+                }
+            }
+            iterpos = maxIdx;
+        }
+
+        if (iterpos < size[0]) {
+            // Iterate along row iterpos.
+        } else {
+            // Iterate along column iterpos - m.
+        }
+    }
 
     float elements[][];
-    unsigned short x, y;
 
+private:
+    // Helper function intended to be compiled with optimization to trim out 
+    // the switch statement (and vectorize).
+    inline Matrix operation(float c, const char mode) {
+        Matrix A = *this;
+        int[] size = getSize();
+
+        for (int i = 0; i < size[0]; i++) {
+            for (int j = 0; j < size[1]; j++) {
+                switch(mode):
+                case 'a':
+                    A[i][j] += c;
+                    break;
+                case 's':
+                    A[i][j] -= c;
+                    break;
+                case 'm':
+                    A[i][j] *= c;
+                    break;
+                case 'd':
+                    A[i][j] /= c;
+                    break;
+            }
+        }
+
+        return A;
+    }
 }
 
-//================================================================================
-// Basic operations.
-
-bool sameDimensions(Matrix &a, Matrix &b);
-bool isSquare(Matrix &a);
-
-Matrix add(Matrix &a, Matrix &b);
-Matrix subtract(Matrix &a, Matrix &b);
-Matrix multiply(Matrix &a, Matrix &b);
-
-Matrix transpose(Matrix &a);
-
-//================================================================================
-// Selection based operations.
-
-// Returns a matrix containing the selection within a larger matrix.
-Matrix getSubmatrix(Matrix &a, int bounds[]);
-
-// Overwrites a subregion within the matrix with another smaller matrix.
-void pasteSubmatrix(Matrix &a, Matrix &b, int pos[]);
-
-Matrix addSubmatrix(Matrix &a, Matrix &b, int pos[]);
-Matrix subtractSubmatrix(Matrix &a, Matrix &b, int pos[]);
-Matrix multiplySubmatrix(Matrix &a, Matrix &b, int pos[]);
-
-//================================================================================
-// Wait what?
-
-Matrix exp(Matrix a);
+}
